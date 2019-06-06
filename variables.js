e9e40da9ec826b9f3a70a383be539cf6dce28cbc
MaliKyVeLL <emmanuelmushashu@gmail.com>
2019-06-06 23:49:09 +0200

    javascript phase 1

--- variables.js
+++ variables.js
@@ -1,1 +1,12 @@
 // you solution should be here
+var y = 1990;
+var f = 2050;
+var  age = f - y;
+console.log(age)
+
+//solution 2
+var age = 28;
+var maxAge = 100;
+var estAmountPayDay = 10;
+var totalRestlife = (maxAge - age) * 365 * estAmountPayDay;
+console.log(totalRestlife)
